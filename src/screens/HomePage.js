09a04d94158f067f499eea3c8b36e517eb5daba9
hunterkidx <sennett@regentsoft.com.hk>
2023-04-15 02:16:48 +0800

    adding all course detail

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { ScrollView } from 'react-native';
 
 // Define the course schedule data
 const scheduleData = [
-  { date: '2022-04-18', course: 'Math 101', location: 'Room 101', lecturer: 'John Doe' },
-  { date: '2022-04-19', course: 'Science 201', location: 'Room 201', lecturer: 'Jane Doe' },
-  { date: '2022-04-20', course: 'History 301', location: 'Room 301', lecturer: 'Bob Smith' },
-  { date: '2022-04-21', course: 'English 401', location: 'Room 401', lecturer: 'Alice Brown' },
-  { date: '2022-04-22', course: 'Art 501', location: 'Room 501', lecturer: 'David Lee' },
-  { date: '2022-04-22', course: 'Music 601', location: 'Room 601', lecturer: 'Emily Davis' },
+  { date: '2022-04-18', course: 'Math 101', time: '9:00 AM - 10:30 AM', location: 'Room 101', lecturer: 'John Doe' },
+  { date: '2022-04-19', course: 'Science 201', time: '11:00 AM - 12:30 PM', location: 'Room 201', lecturer: 'Jane Doe' },
+  { date: '2022-04-20', course: 'History 301', time: '2:00 PM - 3:30 PM', location: 'Room 301', lecturer: 'Bob Smith' },
+  { date: '2022-04-21', course: 'English 401', time: '10:00 AM - 11:30 AM', location: 'Room 401', lecturer: 'Alice Brown' },
+  { date: '2022-04-22', course: 'Art 501', time: '1:00 PM - 2:30 PM', location: 'Room 501', lecturer: 'David Lee' },
+  { date: '2022-04-22', course: 'Music 601', time: '3:00 PM - 4:30 PM', location: 'Room 601', lecturer: 'Emily Davis' },
 ];
 
 // Configure the calendar locale
@@ -64,139 +65,110 @@ const CourseSchedule = () => {
     setSelectedCourse(course);
   };
 
-  // Get all the courses
-  const allCourses = [...new Set(scheduleData.map((course) => course.course))];
+  // Filter the schedule data based on the selected date
+  const filteredScheduleData = selectedDate
+    ? scheduleData.filter((item) => item.date === selectedDate)
+    : scheduleData;
 
   return (
-    <ScrollView contentContainerStyle={styles.contentContainer}>
-      <View style={styles.container}>
-        <Text style={styles.heading}>Course Schedule</Text>
-        <View style={styles.calendarContainer}>
-          <Calendar
-            onDayPress={handleDayPress}
-            markedDates={{
-              [selectedDate]: { selected: true },
-              ...scheduleData.reduce((acc, course) => {
-                acc[course.date] = { marked: true };
-                return acc;
-              }, {}),
-            }}
-          />
-        </View>
-        <View style={styles.scheduleContainer}>
-          {selectedDate && (
-            <>
-              <Text style={styles.dateHeading}>{selectedDate}</Text>
-              {scheduleData
-                .filter((course) => course.date === selectedDate)
-                .map((course) => (
-                  <TouchableOpacity
-                    key={course.course}
-                    style={[
-                      styles.courseContainer,
-                      selectedCourse === course && styles.selectedCourse,
-                    ]}
-                    onPress={() => handleCoursePress(course)}
-                  >
-                    <Text style={styles.courseName}>{course.course}</Text>
-                    <Text style={styles.courseInfo}>
-                      {course.location} | {course.lecturer}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
-            </>
-          )}
-          {selectedCourse && (
-            <TouchableOpacity
-              style={styles.courseDetailContainer}
-              onPress={() => console.log(`Navigate to ${selectedCourse.course} detail page`)}
-            >
-              <Text style={styles.courseDetailText}>View Course Details</Text>
-            </TouchableOpacity>
-          )}
-          {!selectedDate && (
-            <>
-              <Text style={styles.dateHeading}>All Courses</Text>
-              {allCourses.map((course) => (
-                <TouchableOpacity
-                  key={course}
-                  style={[
-                    styles.courseContainer,
-                    selectedCourse?.course === course && styles.selectedCourse,
-                  ]}
-                  onPress={() => setSelectedCourse(scheduleData.find(c => c.course === course))}
-                >
-                  <Text style={styles.courseName}>{course}</Text>
-                  <Text style={styles.courseInfo}>
-                    {scheduleData
-                      .filter((c) => c.course === course)
-                      .map((c) => `${c.location} | ${c.lecturer}`)
-                      .join('\n')}
-                  </Text>
-                </TouchableOpacity>
-              ))}
-            </>
-          )}
-        </View>
+    <View style={styles.container}>
+      <View style={styles.calendarContainer}>
+        <Calendar onDayPress={handleDayPress} />
       </View>
-    </ScrollView>
+      <View style={styles.scheduleContainer}>
+        <ScrollView>
+          <View style={styles.allCoursesContainer}>
+            <Text style={styles.allCoursesHeader}>All Courses</Text>
+            {scheduleData.map((item, index) => (
+              <TouchableOpacity key={index} onPress={() => handleCoursePress(item.course)}>
+                <View style={styles.courseOverviewContainer}>
+                  <Text style={styles.courseOverviewText}>{item.course}</Text>
+                  <Text style={styles.courseOverviewText}>{item.time}</Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+          {filteredScheduleData.map((item, index) => (
+            <TouchableOpacity key={index} onPress={() => handleCoursePress(item.course)}>
+              <View style={styles.courseContainer}>
+                <Text style={styles.courseText}>{item.course}</Text>
+                <Text style={styles.courseText}>{item.time}</Text>
+                <Text style={styles.courseText}>{item.location}</Text>
+                <Text style={styles.courseText}>{item.lecturer}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+          {selectedCourse && (
+            <View style={styles.selectedCourseContainer}>
+              <Text style={styles.selectedCourseText}>Selected Course: {selectedCourse}</Text>
+            </View>
+          )}
+        </ScrollView>
+      </View>
+    </View>
   );
 };
 
-// Define the styles
+// Define the styles for the components
 const styles = StyleSheet.create({
-  contentContainer: {
-    flexGrow: 1,
-    paddingBottom: 20,
-  },
   container: {
     flex: 1,
-    paddingTop: 50, // Add paddingTop to push the content down
-    paddingHorizontal: 20,
+    flexDirection: 'column',
   },
   calendarContainer: {
-    marginBottom: 20,
+    flex: 2,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
   },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+  scheduleContainer: {
+    flex: 2,
+    backgroundColor: '#FFF',
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    paddingLeft: 0,
   },
-  dateHeading: {
+  allCoursesContainer: {
+    paddingLeft: 20,
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
+  },
+  allCoursesHeader: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  scheduleContainer: {
-    flex: 1,
+  courseOverviewContainer: {
+    flexDirection: 'column',
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
   },
-  courseContainer: {
-    backgroundColor: '#f5f5f5',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  selectedCourse: {
-    backgroundColor: '#c9c9c9',
-  },
-  courseName: {
-    fontWeight: 'bold',
+  courseOverviewText: {
     fontSize: 16,
     marginBottom: 5,
   },
-  courseInfo: {
-    fontSize: 14,
+  courseContainer: {
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
   },
-  courseDetailContainer: {
-    backgroundColor: '#c9c9c9',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  courseDetailText: {
-    fontWeight: 'bold',
+  courseText: {
     fontSize: 16,
-    textAlign: 'center',
+    marginBottom: 5,
+  },
+  selectedCourseContainer: {
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+  selectedCourseText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
